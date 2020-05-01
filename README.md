0ae0f510fa0264d6e3a8d38a2df702a829e32e53
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-05-01 00:52:27 -0500

    :rocket: Add Project notebooks with testing scrappers

---
+++ README.md
@@ -0,0 +1,22 @@
+# Web Scraping en la web
+
+Probando herramientas de Scraping como:
+
+* Request y BS4
+* Selenium (JS Sites)
+* Scrapy
+* API Spotify
+* Tesseract (OCR)
+* Proxies
+
+# Herramientas de desarrollo usadas
+* Pycharm
+* Python 3
+* Anaconda
+* Jupyter Notebook
+
+# Preview
+
+<div align="center">
+  <img src="img/1.png">
+</div>
---
+++ notebooks/scrapy.py
@@ -0,0 +1,48 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+
+class Spider12(scrapy.Spider):
+    name = 'spider12'
+    allowed_domains = ['pagina12.com.ar']
+    custom_settings = {'FEED_FORMAT': 'json',
+                       'FEED_URI': 'resultados_scrapy.json',
+                       'DEPTH_LIMIT': 2,
+                       'FEED_EXPORT_ENCODING': 'utf-8'}
+    start_urls = ['https://www.pagina12.com.ar/secciones/el-pais',
+                  'https://www.pagina12.com.ar/secciones/economia',
+                  'https://www.pagina12.com.ar/secciones/sociedad',
+                  'https://www.pagina12.com.ar/suplementos/cultura-y-espectaculos',
+                  'https://www.pagina12.com.ar/secciones/ciencia',
+                  'https://www.pagina12.com.ar/secciones/el-mundo',
+                  'https://www.pagina12.com.ar/secciones/deportes',
+                  'https://www.pagina12.com.ar/secciones/contratapa']
+
+    def parse(self, response):
+        featured_el = response.xpath('//div[@class="featured-article__container"]//h2/a/@href').get()
+        if featured_el is not None:
+            yield response.follow(featured_el, callback=self.parse_new)
+
+        news = response.xpath('//div[@class="article-box__container"]/h2/a/@href').getall()
+        for new in news:
+            yield response.follow(new, callback=self.parse_new)
+
+        next_page = 'https://www.pagina12.com.ar{}'.format(
+            response.xpath('//a[@class="pagination-btn-next"]/@href').get())
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_new(self, response):
+        title = response.xpath('//div[@class="article-titles"]//h1/text()').get()
+        body = ' '.join(response.xpath('//div[@class="article-text"]/p/text()').getall())
+        section = response.xpath('//div[@class="suplement"]/a/text()').get()
+        yield {'section': section,
+               'url': response.url,
+               'title': title,
+               'body': body}
+
+
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(Spider12)
+    process.start()
